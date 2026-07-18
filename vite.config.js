142a4dd566f36ae3563f78e3bc001e36c02f47f0
Uwazie chidubem <uwaziechidubem55@gmail.com>
2026-07-18 15:09:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['flutterwave-react-v3']
+    }
+  },
+  define: {
+    'process.env.NEXT_PUBLIC_FLW_PUBLIC_KEY': JSON.stringify(process.env.NEXT_PUBLIC_FLW_PUBLIC_KEY || '')
   }
 })

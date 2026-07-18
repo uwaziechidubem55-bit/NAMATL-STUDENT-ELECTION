7db1005bdf05b0fa1c3947fd37d088372c75e5f3
Uwazie chidubem <uwaziechidubem55@gmail.com>
2026-07-18 13:50:57 +0100

    Create src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </StrictMode>
+  );
+  console.log('NAMTLS E-Voting System mounted');
+} else {
+  document.body.innerHTML = '<h1>FATAL: root element missing</h1>';
+}

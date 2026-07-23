ff8b8c88e10ed7b32ca568c65424482dab5dfa8e
Uwazie chidubem <uwaziechidubem55@gmail.com>
2026-07-23 18:12:15 +0100

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { db, storage } from '../firebase';
-import { collection, addDoc, getDocs, doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs, getDoc, doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 import { useDataCharge } from '../context/DataChargeContext';
 

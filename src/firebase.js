e3f288ee8f4dbdd824e5895cac2334b002fb090d
Uwazie chidubem <uwaziechidubem55@gmail.com>
2026-07-24 10:08:51 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwdhpv7e0Y3xNBovOpJEpRn9_jmDUOq8E",
-  authDomain: "namtls-voting-2026-2027.firebaseapp.com",
-  projectId: "namtls-voting-2026-2027",
-  storageBucket: "namtls-voting-2026-2027.firebasestorage.app",
-  messagingSenderId: "955792311858",
-  appId: "1:955792311858:web:49a566b351ebab86c05e2b",
-  measurementId: "G-SVDG83TZSG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBwdhpv7e0Y3xNBovOpJEpRn9_jmDUOq8E",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "namtls-voting-2026-2027.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "namtls-voting-2026-2027",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "namtls-voting-2026-2027.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "955792311858",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:955792311858:web:49a566b351ebab86c05e2b"
 };
 
 const app = initializeApp(firebaseConfig);
